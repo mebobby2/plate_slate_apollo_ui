5b81215b648504c16814ff5114b07314e8bc4e15
Bobby <bobby.lei@vodafone.com>
2020-06-05 15:16:29 +1200

    Initial bootstrap of UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  get menuItems() {
+    return [
+      {id: "stub-1", name: "Stub Menu Item 1"},
+      {id: "stub-2", name: "Stub Menu Item 2"},
+      {id: "stub-3", name: "Stub Menu Item 3"},
+    ]
+  }
+
+  renderMenuItem(menuItem) {
+    return (
+      <li key={menuItem.id}>{menuItem.name}</li>
+    )
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.menuItems.map(menuItem => this.renderMenuItem(menuItem))}
+      </ul>
+    )
+  }
 }
 
 export default App;
